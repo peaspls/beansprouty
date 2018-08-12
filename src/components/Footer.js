8120f99dadc5fa5e166fcf9fd0a3789c18e5fdc0
christoffer <christoffer.thondrup@gmail.com>
2018-08-12 17:10:23 +0200

    New Footer and GithubIcon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,45 @@
 import React from 'react'
+import GithubIcon from './GithubIcon'
 
 class Footer extends React.Component {
     render() {
         return (
             <footer
               style={{
-                marginTop: '-24px',
-                padding: '60px 60px',
-                textAlign: 'center',
-                background: '#B8D078'
+                marginTop: -24,
+                padding: '80px 40px',
+                background: 'beige',
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                color: 'rgb(128, 122, 132)'
               }}
             >
-              <p>
-                Icons made by Freepik from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-              </p>
-              <p>
-                Created by Christoffer N. Aa. Thondrup.
-                Code available on <a href="https://github.com/therealice/beansprouty">Github</a>
-              </p>
+              <div style={{
+                margin: 0
+              }}>
+                <h3 style={{
+                  marginTop: 0
+                }}>Words from the creator</h3>
+                Hi!<br />
+                I'm Christoffer N. Aa. Thondrup<br />
+                Thanks for visiting
+              </div>
+              <div style={{
+                margin: 0
+              }}>
+                <a href="https://github.com/therealice/beansprouty" alt="Github" style={{
+                  textDecoration: 'none',
+                  color: 'rgb(128, 122, 132)',
+                  width: 60,
+                  display: 'inline-block',
+                  textAlign: 'center'
+                }}>
+                  <GithubIcon />
+                  <span style={{
+                    display: 'block'
+                  }}>Code on Github</span>
+                </a>
+              </div>
             </footer>
         )  
     }
